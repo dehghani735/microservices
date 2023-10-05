5c1a00bfeb9c398a521c197a89b995f6514ba827
mr.detafti <dehghani735@gmail.com>
2023-10-05 13:59:17 +0330

    Maven plugin configured.

--- notification/src/main/java/com/detafti/apigw/notification/NotificationService.java
+++ notification/src/main/java/com/detafti/apigw/notification/NotificationService.java
@@ -1,5 +1,27 @@
 package com.detafti.apigw.notification;
 
+import com.detafti.apigw.clients.notification.NotificationRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    public void send(NotificationRequest notificationRequest) {
+        notificationRepository.save(
+                Notification.builder()
+                        .toCustomerId(notificationRequest.toCustomerId())
+                        .toCustomerEmail(notificationRequest.toCustomerName())
+                        .sender("Amigoscode")
+                        .message(notificationRequest.message())
+                        .sentAt(LocalDateTime.now())
+                        .build();
+        );
+    }
 
 }
